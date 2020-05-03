f438f0f31f0a8294e03fd550cf8a9bc4c9a495c8
je-r <5433476+je-r@users.noreply.github.com>
2020-05-04 01:28:09 +0200

    added nuget and using

--- GraphQL-ASP.NET-WebAPI/Controllers/GraphQLController.cs
+++ GraphQL-ASP.NET-WebAPI/Controllers/GraphQLController.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using GraphQL.Instrumentation;
+using GraphQL.NewtonsoftJson;
+using GraphQL.Types;
+using GraphQL.Validation.Complexity;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace GraphQL_ASP.NET_WebAPI.Controllers
